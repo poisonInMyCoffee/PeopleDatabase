187bd5285698a09d7bdc412d42f03a202905fbb2
Utkarsh Tripathi <shandilya0103@gmail.com>
2025-03-21 18:55:53 +0530

    EF core environment setup

--- CRUDPeople/Program.cs
+++ CRUDPeople/Program.cs
@@ -1,5 +1,7 @@
+using Microsoft.EntityFrameworkCore;
 using ServiceContracts;
 using Services;
+using Entities;
 
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllersWithViews();
@@ -8,6 +10,18 @@ builder.Services.AddControllersWithViews();
 builder.Services.AddSingleton<ICountriesService, CountriesService>();
 builder.Services.AddSingleton<IPersonsService, PersonsService>();
 
+builder.Services.AddDbContext<PersonsDbContext>(
+    options =>
+    {
+        options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));  //To show that we are using sql server 
+    }
+    );
+
+//connection string
+//Data Source=(localdb)\Begum;Initial Catalog=PersonsDatabase;
+//Integrated Security=True;Connect Timeout=30;
+//Encrypt=False;Trust Server Certificate=False;
+//Application Intent=ReadWrite;Multi Subnet Failover=False
 
 var app = builder.Build();
 
