d5179544e06e407655948b36bb01903cd1f56640
Utkarsh Tripathi <shandilya0103@gmail.com>
2025-04-03 18:57:20 +0530

    added migration

--- Entities/PersonsDbContext.cs
+++ Entities/PersonsDbContext.cs
@@ -41,6 +41,12 @@ namespace Entities
 
             foreach (Person person in persons) {
                 modelBuilder.Entity<Person>().HasData(person);
+
+                //FluentAPI
+                modelBuilder.Entity<Person>().Property(temp => temp.TIN)
+                    .HasColumnName("TaxIdentificationNumber")
+                    .HasColumnType("varchar(8)")
+                    .HasDefaultValue("ABC12345");
             }
         }
         public List <Person> sp_GetAllPersons()
