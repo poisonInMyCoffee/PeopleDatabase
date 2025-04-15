878291afefd98f1f1bb00dcb78fe0b49f64e4e88
Utkarsh Tripathi <shandilya0103@gmail.com>
2025-04-15 15:45:27 +0530

    created controller test for create action method

--- CRUDTests/PersonsControllerTest.cs
+++ CRUDTests/PersonsControllerTest.cs
@@ -32,10 +32,10 @@ namespace CRUDTests
             _fixture = new Fixture();
 
             _countriesServiceMock = new Mock<ICountriesService>();
-            _personsServiceMock= new Mock<IPersonsService>();
+            _personsServiceMock = new Mock<IPersonsService>();
 
             _countriesService = _countriesServiceMock.Object;
-            _personsService = _personsServiceMock.Object; 
+            _personsService = _personsServiceMock.Object;
 
         }
         #region Index
@@ -44,7 +44,7 @@ namespace CRUDTests
         public async Task Index_ShouldReturnIndexViewWithPersonsList()
         {
             //Arrange
-            List<PersonResponse> persons_response_list=_fixture.Create<List<PersonResponse>>();
+            List<PersonResponse> persons_response_list = _fixture.Create<List<PersonResponse>>();
             PersonsController personsController = new PersonsController(_personsService, _countriesService);
             {
                 //Index action method uses 2 methods, of which we create setup/test them
@@ -53,7 +53,7 @@ namespace CRUDTests
                     It.IsAny<string>(), It.IsAny<SortOrderOptions>())).ReturnsAsync(persons_response_list);
 
                 //Act
-                IActionResult result=await personsController.Index(_fixture.Create<string>(), _fixture.Create<string>(), _fixture.Create<string>()
+                IActionResult result = await personsController.Index(_fixture.Create<string>(), _fixture.Create<string>(), _fixture.Create<string>()
                     , _fixture.Create<SortOrderOptions>());
 
                 //Assert
@@ -64,5 +64,62 @@ namespace CRUDTests
 
         }
         #endregion
+
+        #region create
+        [Fact]
+        public async void Create_IfModelError_ToReturnCreateView()
+        {
+            //Arrange
+            PersonResponse persons_response = _fixture.Create<PersonResponse>();
+
+            PersonAddRequest persons_add_request = _fixture.Create<PersonAddRequest>();
+
+            List<CountryResponse> countries = _fixture.Create<List<CountryResponse>>();
+
+            _countriesServiceMock.Setup(temp => temp.GetAllCountries()).ReturnsAsync(countries);
+
+            _personsServiceMock.Setup(temp => temp.AddPerson(It.IsAny<PersonAddRequest>())).ReturnsAsync(persons_response);
+
+            PersonsController personsController = new PersonsController(_personsService, _countriesService);
+
+            //Act
+            personsController.ModelState.AddModelError("PersonName", "Person Name can't be blank");
+
+            IActionResult result = await personsController.Create(persons_add_request);
+
+            //Assert
+            ViewResult viewResult = Assert.IsType<ViewResult>(result);
+            viewResult.ViewData.Model.Should().BeAssignableTo<PersonAddRequest>();
+            viewResult.ViewData.Model.Should().Be(persons_add_request);
+        }
+        [Fact]
+        public async void Create_IfNoModelError_ToReturnRedirectToIndex()
+        {
+            //Arrange
+            PersonAddRequest persons_add_request = _fixture.Create<PersonAddRequest>();
+
+            PersonResponse persons_response = _fixture.Create<PersonResponse>();
+
+            List<CountryResponse> countries = _fixture.Create<List<CountryResponse>>();
+
+            _countriesServiceMock.Setup(temp => temp.GetAllCountries()).ReturnsAsync(countries);
+
+            _personsServiceMock.Setup(temp => temp.AddPerson(It.IsAny<PersonAddRequest>())).ReturnsAsync(persons_response);
+
+            PersonsController personsController = new PersonsController(_personsService, _countriesService);
+
+            //Act
+
+            IActionResult result = await personsController.Create(persons_add_request);
+
+            //Assert
+            RedirectToActionResult redirectResult = Assert.IsType<RedirectToActionResult>(result);
+
+            redirectResult.ActionName.Should().Be("Index");
+        }
+        #endregion  
+
     }
 }
+
+
